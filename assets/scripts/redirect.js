bb4d6513c9b1f9a8ca811d3abec7dbde6a171eb6
oxycodonelol <121684123+oxycodonelol@users.noreply.github.com>
2025-06-07 19:33:19 +0200

    Delete assets directory

--- assets/scripts/redirect.js
+++
@@ -1,8 +0,0 @@
-window.onload = function () {
-  var currentURL = window.location.href;
-
-  if (currentURL.endsWith(".html")) {
-    var newURL = currentURL.substring(0, currentURL.length - 5);
-    window.location.replace(newURL);
-  }
-};
